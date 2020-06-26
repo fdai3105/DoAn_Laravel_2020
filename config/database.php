79db7459170c0a5ae418793d30cf3510b4a35412
ff3105 <hoangphidai123@gmail.com>
2020-06-26 11:53:47 +0700

    Update database connection

--- config/database.php
+++ config/database.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Str;
 
 return [
+    $DATABASE_URL=parse_url("postgres://nlcxfbvuqcajrw:2ddb6951134bb71767d4ba1503e499cbcba56d6cebf4cbe0d41248feb636f1fd@ec2-52-20-248-222.compute-1.amazonaws.com:5432/d4luoompcds2tl"),
 
     /*
     |--------------------------------------------------------------------------
@@ -15,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +67,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], '/'),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
