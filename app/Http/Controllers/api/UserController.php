ace7038875c7fb286c3d537d54780f8be594292e
ff3105 <hoangphidai123@gmail.com>
2020-07-01 16:49:37 +0700

    update api login and reg

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserController extends Controller
@@ -27,11 +28,14 @@ class UserController extends Controller
     {
         $credentials = $request->only('name', 'password');
         $token = null;
-       
-        if (!$token = JWTAuth::attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_email_or_password'], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['failed_to_create_token'], 500);
         }
-        return response()->json(compact('token'));
+        return response()->json(compact('token'), 200);
     }
 
     public function register(Request $request)
